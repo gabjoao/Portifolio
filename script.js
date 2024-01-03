f7952f38e713e7fd6976bffb1f059f45d19e1152
gabjoao <joao.25.ribeiro01@gmail.com>
2024-01-03 15:57:45 -0300

    feat: :sparkles: Informações mais específicas para cada projeto

--- script.js
+++ script.js
@@ -42,3 +42,50 @@ setInterval(function(){
     }
 //500
 }, 500)
+
+class Projeto{
+    constructor(titulo, img1, img2, p, info, link){
+        this.titulo = titulo;
+        this.img1 = img1;
+        this.img2 = img2;
+        this.p = p;
+        this.info = info;
+        this.link = link;
+    }
+}
+
+var Projetos = [];
+Projetos.push(new Projeto("Imobiliária Fachada", "media/projeto1.png", "media/projeto1-2.png" ,"O projeto consiste na utilização de classes e objetos em JavaScript para a criação de imóveis. Após o usuário cadastrar, o imóvel aparecerá na página e ficará salvo no localStorage toda vez que o usuário entrar novamente.", "out 2023 / Imobiliária Fachada / João Gabriel / out 2023", "https://gabjoao.github.io/Imobiliaria-Fachada/"));
+
+Projetos.push(new Projeto("Serene", "media/projeto2.png", "media/projeto2-2.png", "Uma página de E-commerce de uma marca fictícia. A página possui quatro seções, além de informações específicas para cada produto com opções para compras.", "set 2023 / Serene / João Gabriel / set 2023", "https://gabjoao.github.io/landing-page-serene/"));
+
+Projetos.push(new Projeto("Life is Strange", "media/projeto3.png", "media/projeto3-2.png", "Uma Landing Page informativa sobre a saga de jogos Life is Strange. Possui as informações principais do jogo, além de páginas específicas para cada um dos jogos e uma página de contato.", "set 2023 / Life is Strange / João Gabriel / set 2023", "https://gabjoao.github.io/Life-is-Strange-Page/"));
+
+Projetos.push(new Projeto("MiauAu", "media/projeto4.png", "media/projeto4-2.png", "Uma página informativa sobre cachorros e gatos que consome APIs para informações de raças. Possui uma seção de dicas para adoção, além das páginas espefícas para os animais e uma galeria para cada.", "nov 2023 / MiauAu / João Gabriel / nov 2023", "https://gabjoao.github.io/MiauAu/"));
+
+function infos(index){
+   document.querySelector("#info").style.display = "flex";
+
+    var titulo = document.querySelector(".info-titulo");
+    titulo.textContent = Projetos[index].titulo;
+
+    var img1 = document.querySelector(".info-img1");
+    img1.src = Projetos[index].img1;
+
+    var img2 = document.querySelector(".info-img2");
+    img2.src = Projetos[index].img2;
+
+    var p = document.querySelector(".info-p");
+    p.textContent = Projetos[index].p;
+
+    var infos = document.querySelector(".p-info");
+    infos.textContent = Projetos[index].info;
+
+    var btn = document.querySelector(".info-btn");
+    btn.href = Projetos[index].link;
+
+}
+
+function fechar(){
+    document.querySelector("#info").style.display = "none";
+}
